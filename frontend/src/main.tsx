c3064c0c499c4bdde1a18df5af28c249c2c8e6ff
kenwong <20069234+kenwong9508@users.noreply.github.com>
2026-04-23 18:39:48 +0800

    chore: delete App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import "./index.css";
 import { ToastProvider } from "./components/ToastProvider";
+import { AppRouter } from "./routes/Router";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
-        <App />
+        <AppRouter />
       </ToastProvider>
     </QueryClientProvider>
   </React.StrictMode>,
